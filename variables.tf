df4ea40fc5e4ba7cf9627328d0845a9da85f51cd
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-03-24 23:13:30 +0100

    first

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+  default = "/root/pets.txt"
+}
+variable "content" {
+  default = "We love pets! My favourite pet is no pet!"
+}
+variable "prefix" {
+  default = "Mrs"
+}
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = "2"
+}
